4b75d80ead7e632ac89e8c131753446cf54e980d
soyoung <soyoung47@gmail.com>
2020-07-08 15:34:54 +0900

    blabla

---
+++ bitmap.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stlib.h>
+
+
+int checkb(int fd, int iorb){
+	if (i == 0){ //for inode bitmap
+		lseek(fd,1024, SEEK_SET);
+		for(int i = 0; i<11	
+	}
+	else if (i == 1){ //for data bitmap
+	}
+	else{
+		printf("Parameter should be 0 or 1!");
+	}
+
+}
+
