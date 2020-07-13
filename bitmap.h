2be3bf2433ca0e0554c62228b5e58b778500e70d
h0han <hyhan@likelion.org>
2020-07-13 15:22:44 +0900

    200713

---
+++ bitmap.h
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+int checkb(int iorb){
+    int fd;
+    unsigned char* buf;
+    unsigned char comp;
+    int count;
+    int byte;
+}
+
+void chanb(int num, int iorb){
+    int fd;
+    unsigned char* buf;
+    unsigned char com;
+    unsigned char new;
+    int byte;
+}
+
+void main(){
+}
