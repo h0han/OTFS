60d63decd67d4c1e5fce3d43f068db766aa3b201
soyoung172 <61623279+soyoung172@users.noreply.github.com>
2020-07-22 16:15:34 +0900

    Update bitmap.c

--- bitmap.c
+++ bitmap.c
@@ -5,37 +5,43 @@
 #include <fcntl.h>
 
 int checkb(int iorb){
-	int fd = open("region", O_RDWR|O_CREAT, 0666);
-	if (iorb == 0) { //for inode bitmap
-        	lseek(fd, 1024, SEEK_SET);
-        	unsigned char* buf = malloc(1024);
-        	read(fd, buf, 1024);
-        	for (int byte = 0; byte < 1024; byte++) {
-	            	unsigned char comp = 128;
-        	    	int count = 0;
-        		if (buf[byte] != 255) {
-                		while ((buf[byte] & comp)!=0) {
-	                	    comp = comp >> 1;
-	        	            count++;
-        	        	}
-	        	        return byte * 8 + count;
-        	    	}
-        	}
-    	}	
+        int fd = open("region", O_RDWR|O_CREAT, 0666);
+        if (iorb == 0) { //for inode bitmap
+                lseek(fd, 1024, SEEK_SET);
+                unsigned char* buf = malloc(1024);
+                read(fd, buf, 1024);
+                for (int byte = 0; byte < 1024; byte++) {
+                        unsigned char comp = 128;
+                        int count = 0;
+                        if (buf[byte] != 255) {
+                                while ((buf[byte] & comp)!=0) {
+                                    comp = comp >> 1;
+                                    count++;
+                                }
+                                free(buf);
+                                close(fd);
+                                return  byte * 8 + count;
+                        }
+                }
+
+        }
+	
 	else if (iorb == 1){ //for data bitmap
                 lseek(fd,2048, SEEK_SET);
                 unsigned char* buf = malloc(1024*128);
                 read(fd, buf, 1024*128);
                 for(int byte = 0; byte<1024*128;byte++){
-                        unsigned char comp;
+                        unsigned char comp = 128;
                         int count = 0;
-                        comp = 128;
-                        while((buf[byte]&comp)!=0){
-                        	comp = comp>>1;
-                               	count++;
+                        if (buf[byte] != 255) {
+                                while ((buf[byte] & comp)!=0) {
+                                    comp = comp >> 1;
+                                    count++;
+                                }
+                                free(buf);
+                                close(fd);
+                                return  byte * 8 + count;
                         }
-		free(buf);
-                return byte*8+count;
 		}	
 	}
 	else{
@@ -45,22 +51,24 @@ int checkb(int iorb){
 }
 
 void chanb(int num, int iorb) {
-	int fd = open("region", O_RDWR|O_CREAT, 0666);
-	unsigned char* buf = malloc(1);
-	if (iorb == 0) {
-		lseek(fd, 1024 + (num/8) * 512, SEEK_SET);
-	}
-	else {
-		lseek(fd, 2048 + (num/8) * 4096, SEEK_SET);
-	}
-	read(fd, buf, 1);
-	lseek(fd, -1, SEEK_CUR);
-	unsigned char com = num % 8;
-	com = 128 >> com;
-	unsigned char new = buf[0] ^ com;
-	buf[0] = new;
-	write(fd, buf, 1);
-	free(buf);
+        int fd = open("region", O_RDWR|O_CREAT, 0666);
+        unsigned char* buf = malloc(1);
+        if (iorb == 0) {
+                lseek(fd, 1024 + (num/8), SEEK_SET);
+        }
+        else {
+                lseek(fd, 2048 + (num/8), SEEK_SET);
+        }
+        read(fd, buf, 1);
+        lseek(fd, -1, SEEK_CUR);
+        unsigned char com = num % 8;
+        com = 128 >> com;
+        unsigned char new = buf[0] ^ com;
+        buf[0] = new;
+        write(fd, buf, 1);
+        free(buf);
+        close(fd);
 }
+
 
 
