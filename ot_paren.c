07975ddd89364056bdad2c4760498aed14b46946
h0han <hyhan@likelion.org>
2020-08-12 22:08:22 +0900

    Modify

--- ot_paren.c
+++ ot_paren.c
@@ -3,7 +3,7 @@
 #include <string.h>
 
 char* ot_paren(const char* path) {
-	char* par_path;
+	char par_path[100] = "";
 	char* ptr = strtok(path, '/');
 	int num_file = count(path, '/');
 	for (int i = 0; i < num_file; i++) {
