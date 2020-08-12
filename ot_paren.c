53c365b1118ef59e91ac2544aca5f8bd086015ed
h0han <hyhan@likelion.org>
2020-08-12 21:59:40 +0900

    ot_paren

---
+++ ot_paren.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+
+char* ot_paren(const char* path) {
+	char* par_path;
+	char* ptr = strtok('/', path);
+	int num_file = count(path, '/');
+	for (int i = 0; i < num_file; i++) {
+		strcat(par_path, '/');
+		strcat(par_path, ptr);
+		ptr = strtok(NULL, '/');
+	}
+
+	return par_path;
+}
+
+
